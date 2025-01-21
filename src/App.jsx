ff444aa75883cab03501c5a575984b3eb2f22a24
LouisMbc <louis.malbec@ecole-hexagone.com>
2025-01-21 15:03:49 +0100

    du style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router'; 
+import { BrowserRouter } from 'react-router';
 import { Header } from './components/layouts/Header';
 import { Footer } from './components/layouts/Footer';
 import { AuthProvider } from './auth/AuthProvider.jsx';
@@ -11,7 +11,7 @@ const App = () => {
       <BrowserRouter>
         <div className="d-flex flex-column vh-100">
           <Header />
-          <main className="flex-grow-1">
+          <main className="flex-grow-1" style={{ paddingTop: '76px' }}>
             <Router />
           </main>
           <Footer />
